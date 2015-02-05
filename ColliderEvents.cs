07688e62522660ab65c42a326d87fe70d6494b84
d-lask <dlaskey92@gmail.com>
2015-02-04 20:15:09 -0600

    Added ColliderEvents.cs

---
+++ ColliderEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Put this script on a gameobject with a collider and 
+/// subscribe to the delegates to receive notifications of the different collider events!
+/// </summary>
+public class ColliderEvents : MonoBehaviour 
+{
+	public Action<Collision> CollisionEnter = delegate { };
+    public Action<Collision> CollisionStay = delegate { };
+    public Action<Collision> CollisionExit = delegate { };
+
+    public Action<Collider> TriggerEnter = delegate { };
+    public Action<Collider> TriggerStay = delegate { };
+    public Action<Collider> TriggerExit = delegate { };
+
+	void OnCollisionEnter(Collision other)
+	{
+		CollisionEnter(other);
+	}
+	
+	void OnCollisionStay(Collision other)
+	{
+		CollisionStay(other);
+	}
+	
+	void OnCollisionExit(Collision other)
+	{
+		CollisionExit(other);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		TriggerEnter(other);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		TriggerStay(other);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		TriggerExit(other);
+	}
+}
